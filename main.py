a89a1b5cb393eaf74cac80681f2511fdee260f0f
Luiz-Henrique03 <cumiranga@gmaail.com>
2025-12-01 21:12:01 -0300

    Definido função principal

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import databaseManager
 
 Datamanager = databaseManager.DatabaseManager
 
-name = input("Digite o nome do jogador: ")
-position = input("Digite a posição do jogador: ")
-score = float(input("Digite a nota do jogador: "))
-player = players.Player(name, position, score)
-Datamanager.insert_player(player, position)
+
+def set_lineup():
+    name = input("Digite o nome do jogador: ")
+    position = input("Digite a posição do jogador: ")
+    score = float(input("Digite a nota do jogador: "))
+    player = players.Player(name, position, score)
+    Datamanager.insert_player(player, position)
 
         
     
