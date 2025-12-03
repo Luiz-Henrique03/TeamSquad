a1d296a983279b5d1a7bf560c759bdf032fbf070
Luiz-Henrique03 <cumiranga@gmaail.com>
2025-12-02 21:06:18 -0300

    Convocação de goleiros

--- main.py
+++ main.py
@@ -5,11 +5,27 @@ Datamanager = databaseManager.DatabaseManager
 
 
 def set_lineup():
-    name = input("Digite o nome do jogador: ")
-    position = input("Digite a posição do jogador: ")
-    score = float(input("Digite a nota do jogador: "))
-    player = players.Player(name, position, score)
-    Datamanager.insert_player(player, position)
-
+    i = 1
+    print("Convoque sua seleção ")
+    while i < 3:
+        if i < 1:
+            i = 1
+        name = input("Nome goleiro: ")
+        if name == "": 
+            print("Entrada inválida. Tente novamente.")
+            i = i - 1
+            print(i)
+        elif name != "":
+            position = "Goleiro"
+            score = float(input("Digite a nota do jogador: "))
         
+            player = players.Player(name, position, score)
+            Datamanager.insert_player(player, position)
+            i+=1
+        
+      
+
+    
+
+set_lineup()     
     
