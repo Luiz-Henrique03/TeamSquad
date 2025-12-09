8efa86c704a6cd9df62e317636c07292e290a79f
Luiz-Henrique03 <cumiranga@gmaail.com>
2025-12-08 23:29:02 -0300

    Feito função para laterais

--- main.py
+++ main.py
@@ -3,25 +3,48 @@ import databaseManager
 
 Datamanager = databaseManager.DatabaseManager
 
+def Goleiros():
+    i = 1
+    while i < 4:
+            if i < 1:
+                i = 1
+            name = input("Nome goleiro: ")
+            if name == "": 
+                print("Entrada inválida. Tente novamente.")
+                i = i - 1
+                print(i)
+            elif name != "":
+                position = "Goleiro"
+                score = float(input("Digite a nota do jogador: "))
+            
+                player = players.Player(name, position, score)
+                Datamanager.insert_player(player, position)
+                i+=1
+
+def Laterais():
+    i = 1
+    while i < 5:
+            if i < 1:
+                i = 1
+            name = input("Nome Lateral: ")
+            if name == "": 
+                print("Entrada inválida. Tente novamente.")
+                i = i - 1
+                print(i)
+            elif name != "":
+                position = "Lateral"
+                score = float(input("Digite a nota do jogador: "))
+            
+                player = players.Player(name, position, score)
+                Datamanager.insert_player(player, position)
+                i+=1
 
 def set_lineup():
     i = 1
     print("Convoque sua seleção ")
-    while i < 3:
-        if i < 1:
-            i = 1
-        name = input("Nome goleiro: ")
-        if name == "": 
-            print("Entrada inválida. Tente novamente.")
-            i = i - 1
-            print(i)
-        elif name != "":
-            position = "Goleiro"
-            score = float(input("Digite a nota do jogador: "))
-        
-            player = players.Player(name, position, score)
-            Datamanager.insert_player(player, position)
-            i+=1
+    Goleiros()
+    Laterais()
+    
         
       
 
